7bc3c028144c66a62d9d9977d04111bea08540db
mplatts <plattsmw@gmail.com>
2010-08-05 14:01:34 +1000

    installed cucumber and rspec - rspec now configured to work with capybara and haml

---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+# This file is copied to ~/spec when you run 'ruby script/generate rspec'
+# from the project root directory.
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/dsl' 
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+
+  config.include(Capybara, :type => :integration) 
+  config.include Haml::Helpers
+  config.include ActionView::Helpers
+  config.before(:each) do
+    init_haml_helpers
+  end
+
+  config.mock_with :rspec
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, comment the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+end
